3e1e2c50c70454891050b7e2b125bdaf4518a559
afeastforcrows <afeastforcrows@gmail.com>
2015-09-16 22:56:04 -0700

    Adding picking

---
+++ picking_glfw.d
@@ -0,0 +1,2 @@
+picking_glfw.o: picking_glfw.cpp GLFWApp.h GFXMath.h GLSLShader.h \
+  transformations.h glut_teapot.h
---
+++ transformations.d
@@ -0,0 +1,2 @@
+transformations.o: transformations.cpp transformations.h GFXMath.h \
+  transformations_solution.cpp
